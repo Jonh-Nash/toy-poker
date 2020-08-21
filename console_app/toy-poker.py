ce68241c85cc502ddac5a741d03a57b1126d7322
Jonh-Nash <vicious8424687@gmail.com>
2020-08-21 20:01:55 +0900

    add main file of toy-poker game

---
+++ console_app/toy-poker.py
@@ -0,0 +1,12 @@
+def main():
+    turn = 1
+    player_money = 10
+    while(turn < 101):
+        print('turn :', turn)
+        print('money:', player_money)
+        turn += 1
+        input('go to next')
+    print('thank you')
+
+if __name__ == '__main__':
+    main()
