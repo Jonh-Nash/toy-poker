7fec9e25d1d6fdfd54fff1e94d65e5d552ac644a
Jonh-Nash <vicious8424687@gmail.com>
2020-10-23 11:30:14 +0900

    stop accounts app and prepare new account system

--- toy_poker/toy_poker/settings.py
+++ toy_poker/toy_poker/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+#AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
