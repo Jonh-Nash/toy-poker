e495f698cff1c729f30ecee2d046636809bf26a6
Jonh-Nash <vicious8424687@gmail.com>
2020-10-26 20:31:00 +0900

    fix signup.html at templates and test branch by method

--- toy_poker/poker/views.py
+++ toy_poker/poker/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def signupfunc(request):
+    if request.method == 'POST':
+        print('this is post method')
+    else:
+        print('this is not post method')
     return render(request, 'signup.html', {'some': 200}) 
