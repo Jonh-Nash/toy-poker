3c903c916b7b096fa24314d2075754cf90a3e61d
Jonh-Nash <vicious8424687@gmail.com>
2020-10-26 20:42:59 +0900

    update signup.html and confirm post method in django

--- toy_poker/poker/views.py
+++ toy_poker/poker/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 def signupfunc(request):
+    # actionで遷移先を空、つまり自ページにする。
+    # 自分に送りつけてからページ遷移する処理をこちらで書く
     if request.method == 'POST':
-        print('this is post method')
-    else:
-        print('this is not post method')
+        username = request.POST['username']
+        print(username)
+        return render(request, 'signup.html', {'some': 200}) 
+
     return render(request, 'signup.html', {'some': 200}) 
