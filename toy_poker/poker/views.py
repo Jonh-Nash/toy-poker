4eaa821b341834e36a13ccd71176a2fd07a17685
Jonh-Nash <vicious8424687@gmail.com>
2020-10-27 13:14:58 +0900

    add try functions about duplication of name

--- toy_poker/poker/views.py
+++ toy_poker/poker/views.py
@@ -7,9 +7,12 @@ def signupfunc(request):
     user2 = User.objects.get(username='sato')
     print(user2)
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some': 200}) 
-
+        username2 = request.POST['username']
+        password2 = request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, '', password2)
+            return render(request, 'signup.html', {'some': 200}) 
     return render(request, 'signup.html', {'some': 200}) 
